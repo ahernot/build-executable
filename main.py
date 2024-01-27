9d85d48663304f26d255494dfc3b52e1454462eb
ahernot <70817470+ahernot@users.noreply.github.com>
2024-01-27 20:34:16 +0100

    full build (with test commands)

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-import os
-import subprocess
+import argparse
+from builder import run
 
 
 if __name__ == '__main__':
-    print('Hello world')
-    # os.system('sudo nano /usr/local/bin/my_module')
-    # subprocess.run(["sudo", "chmod", "+x", "/Users/anatole/Downloads/my_module5"])
+
+    # Build arguments parser
+    parser = argparse.ArgumentParser(description='Python program executable builder')  # TODO: description
+    parser.add_argument('-i', '--input', help="Path to the Python module's entrypoint", required=True, type=str)
+    parser.add_argument('-o', '--output', help="Path to the executable file to create", required=True, type=str)
+    parser.add_argument('-p', '--python', help="Path to the python kernel to use", type=str)
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode')
+    
+    # Parse command-line arguments
+    args = parser.parse_args()
+
+    # Run
+    run(
+        path_module_entrypoint=args.input,
+        path_exec=args.output,
+        path_python=args.python,
+        verbose=args.verbose
+    )
 
 
-# import os
-# os.chmod(file_path, 0o755)
+# /usr/local/bin/python3.9 /Users/anatole/Documents/GitHub/build-executable/main.py -i "/Users/anatole/Documents/GitHub/build-executable/main.py" -o "/Users/anatole/Downloads/exec1"  #! Build exec1
+# /Users/anatole/Downloads/exec1 -i "/Users/anatole/Documents/GitHub/build-executable/main.py" -o "/Users/anatole/Downloads/exec2" -p "/usr/local/bin/python3.9"  #! Use exec1
+# /Users/anatole/Downloads/exec1 -i "/Users/anatole/Documents/GitHub/build-executable/main.py" -o "/usr/local/bin/exec2" -p "/usr/local/bin/python3.9"  #! Test SUDO
