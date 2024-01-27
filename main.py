a86af3aa7ae7bec94785ac838a85f60a0b97a675
ahernot <70817470+ahernot@users.noreply.github.com>
2024-01-27 14:58:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import os
+import subprocess
+
 
 if __name__ == '__main__':
-    pass
+    print('Hello world')
+    # os.system('sudo nano /usr/local/bin/my_module')
+    # subprocess.run(["sudo", "chmod", "+x", "/Users/anatole/Downloads/my_module5"])
+
+
+# import os
+# os.chmod(file_path, 0o755)
