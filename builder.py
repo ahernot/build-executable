11cbc57e9c9350dd19ba2bb0fcc1ea8e0b35a45d
ahernot <70817470+ahernot@users.noreply.github.com>
2024-01-27 20:45:27 +0100

    Add checks, warnings, errors, verbose

--- builder.py
+++ builder.py
@@ -1,11 +1,10 @@
 import os
 import subprocess
-
+import warnings
 from src.config import PYTHON_PATH
 
 
-
-def run (path_module_entrypoint: str, path_python: str, path_exec: str):
+def run (path_module_entrypoint: str, path_exec: str, path_python: str = PYTHON_PATH, **kwargs):
     """_summary_
 
     Args:
@@ -14,21 +13,35 @@ def run (path_module_entrypoint: str, path_python: str, path_exec: str):
         path_exec (str): _description_
     """
 
-    # Check permissions
-    sudo = False
+    # Parse arguments
+    verbose = kwargs.get('verbose', False)
     dirpath_exec = os.path.split(path_exec)[0]
+
+    # Check arguments
+    if not os.path.exists(path_python):
+        warnings.warn(f'Invalid Python path. Using default path from config file instead ({PYTHON_PATH})', UserWarning)
+    if not os.path.exists(path_module_entrypoint):
+        warnings.warn(f'Invalid entrypoint path. Executable will not be linked to an existing Python file.', UserWarning)
+        # raise ValueError('Invalid entrypoint path.')
+    if not os.path.exists(dirpath_exec):
+        raise ValueError('Invalid executable path: directory "{dirpath_exec}" does not exist.')
+
+    # Check permissions for executable build path
+    sudo = False
     if not os.access(dirpath_exec, os.W_OK):
         sudo = True
+        if verbose: print(f'Permission required to create executable in {dirpath_exec}.')
 
     # Build executable file
     text = f'#!/bin/bash\n{path_python} {path_module_entrypoint} "\$@"'
     text_escaped = text.replace('"', r'\"')
 
-    command = ['printf', f'"{text_escaped}"', '>', f'"{path_exec}"', '&&', 'chmod', '+x', f'"{path_exec}"']
-    if sudo: command = ['sudo'] + command
-    command_string = ' '.join(command)
-    print(repr(command_string))
-    subprocess.run(command_string, shell=True, check=True)     
-
-    # TODO: command = f'printf "{text_escaped}" | sudo tee "{path_exec}" >/dev/null && sudo chmod +x "{path_exec}"'
-   
+    # Build command
+    if sudo: command = f'printf "{text_escaped}" | sudo tee "{path_exec}" >/dev/null && sudo chmod +x "{path_exec}"'
+    else:    command = f'printf "{text_escaped}" > "{path_exec}" && chmod +x "{path_exec}"'
+    
+    # Run command
+    try:
+        subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError:
+        raise ValueError('Executable creation failed.')
